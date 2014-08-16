a912f4c646604c09bb419c25caf740ba35a5aeb8
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-08-16 15:10:33 -0400

    fact

--- app.py
+++ app.py
@@ -33,6 +33,10 @@ def create():
 		fact=request.form['fact']
 
 		#facts_db.update({'name':list_name}, {'$push' : {fact}})
+		fact_list = facts_db.find_one({'name' : list_name})
+		fact_list['facts'].append(fact);
+		facts_db.update({'name':list_name}, {'$set':fact_list})
+
 		return redirect('/create')
 	return render_template('create.html')
 
