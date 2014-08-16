77a4656857e6b788e43306ff7ff74221a75d43d9
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-08-16 14:21:40 -0400

    twilio update

--- twilio_api.py
+++
@@ -1,6 +0,0 @@
-ACCOUNT_SID = "ACcf72ea65363c8e7585acf4ae877b49ab"
-AUTH_TOKEN = "e61112fee5d7c53cf5983ed9f5c97115"
-FROM_NUMBER = "+12015618743"
-
-def send_message(to_number, body_message):
-	print('hi')
