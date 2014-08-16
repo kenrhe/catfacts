16c077a59a59acaab1b96645796211e67135c409
Robert Kim <rkty13@gmail.com>
2014-08-16 15:12:53 -0400

    hello

---
+++ clock.py
@@ -0,0 +1,26 @@
+from pytz import utc
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.executors.pool import ProcessPoolExecutor
+
+
+jobstores = {
+    'mongo': {'type': 'mongodb'},
+    'default': SQLAlchemyJobStore(os.environ.get('MONGOHQ_URL'))
+}
+executors = {
+    'default': {'type': 'threadpool', 'max_workers': 20},
+    'processpool': ProcessPoolExecutor(max_workers=5)
+}
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 3
+}
+scheduler = BackgroundScheduler()
+
+# .. do something else here, maybe add jobs etc.
+
+scheduler.configure(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
+
+sched.start()
