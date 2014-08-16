cae4b7d5e645283341214f15dba9d170507077f3
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-08-16 15:53:44 -0400

    ‘push’

--- app.py
+++ app.py
@@ -30,9 +30,11 @@ def index():
 def create():
 	if request.method == 'POST':
 		if request.form['submit'] == 'create':
+			pass
 			#fact_list = {'name':request.form['list']}
 			#facts_db.insert(fact_list);
 		elif request.form['submit'] == 'add':
+			pass
 			#list_name = request.form['list']
 			#fact_list = facts_db.find_one({'name':list_name})
 
