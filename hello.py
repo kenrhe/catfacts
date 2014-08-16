4cba08260c61fd16f94beb72a88d896e6206dd56
thejoonshow <thejoonshow@gmail.com>
2014-08-16 12:05:17 -0400

    blah

--- hello.py
+++ hello.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World! Kenneth is awesome. Robert is not. Joon is okay.'
+    return 'Hello World! Kenneth is awesome. Robert is not. Joon is life.'
