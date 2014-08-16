42eb95f105be4ef6a95fb02a6323e9da192897fe
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-08-16 17:13:22 -0400

    ‘push’

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import hashlib
 import datetime
 import pymongo
 import random
-import uuid
 
 from pymongo import MongoClient
 from twilio.rest import TwilioRestClient
@@ -47,7 +46,8 @@ def create():
 				facts_db.insert(fact_list);
 			elif request.form['submit'] == 'add':
 				list_name = request.form['list']
-				facts_db.update({'name':list_name}, {'$push': {'facts':{uuid.uuid4():request.form['fact']}}}, True)
+				fact = request.form['fact']
+				facts_db.update({'name':list_name}, {'$push': {'facts':fact}}, True)
 			return redirect('/create')
 		except Exception, e:
 			print "[EXCEPTION ERROR] " + e
