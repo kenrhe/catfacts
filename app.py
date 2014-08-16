21837e6e050d93d8701f14b9be5d9504e438181e
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-08-16 16:21:52 -0400

    ‘push’

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ facts_db = db.facts
 @app.route('/')
 def index():
 	#send_message('2012500807', 'Ken is so sexy.')
-	return render_template("index.html", facts=facts_db.find())
+	return render_template("index.html", facts=facts_db.find({'name' : 'cats'}))
 
 @app.route('/create', methods=['GET','POST'])
 def create():
